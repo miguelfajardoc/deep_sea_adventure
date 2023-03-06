998efa6a1a8226cb0de19d987c1b47b89c4b8fef
miguelfajardoc <miguelanfac@gmail.com>
2023-03-06 17:40:40 -0500

    first commit

---
+++ buttons.rb
@@ -0,0 +1,27 @@
+
+class Buttons
+
+    attr_accessor :position_x, :position_y, :size_x, :size_y, :image, :process
+
+    def initialize(position_x, position_y, image, &process)
+        @position_x = position_x
+        @position_y = position_y
+        @size_x = image.width
+        @size_y = image.height
+        @image = image
+        @active = true
+        @process = process
+    end
+
+    def clicked?(mouse_x, mouse_y)
+        puts mouse_x, mouse_y
+        puts @position_x, @position_y
+        if mouse_x >= @position_x && mouse_x < (@position_x + size_x)
+            if mouse_y >= @position_y && mouse_y < (@position_y + size_y)
+                return true
+                
+            end
+        end
+        false 
+    end
+end
---
+++ game.rb
@@ -0,0 +1,253 @@
+require 'gosu'
+require_relative 'buttons'
+
+WITH = 600
+HEIGHT = 800
+
+class DeepSeaAdventure < Gosu::Window
+
+    def initialize
+        
+        super WITH, HEIGHT
+        self.caption = "DeepSeaAdventure"
+        @buttons = []
+        @title = Gosu::Image.new("assets/images/title.png")
+        #@background_image = Gosu::Image.new("assets/images/Ocean.png")
+        @background_image = Gosu::Image.from_blob(WITH, HEIGHT, rgba = "\4\105\142\255" * (WITH * HEIGHT))
+        @background_image_2 = Gosu::Image.new("assets/images/submarine_title.png")
+        @start_text = Gosu::Image.from_text("Start", 30, options = {bold: true})
+        @button_start = Buttons.new(250, 400, Gosu::Image.from_blob(150, 40, rgba = "\1\107\140\155" * (150 * 40)), &self.method(:start))
+        @submarine = Gosu::Image.new("assets/images/submarine2.png")
+        @buttons.append(@button_start) 
+        @game = Game.new([Player.new("ass", "blue"), Player.new("aaa", "yellow")])
+        @state = :menu
+    end
+
+    def update
+        if @state == :game
+            p "game"
+        elsif @state == :players
+            generate_players
+            setup
+        else
+        end
+    end
+
+    def setup
+        @game = Game.new(@players)
+    end
+
+    def start
+        puts "start"
+        @state = :game
+    end
+
+    def button_down(id)
+        case id
+        when Gosu::KB_ESCAPE
+            close
+        when Gosu::MS_LEFT
+            element = nil
+            element =  element_clicked
+            unless element.nil?
+                puts element.process
+                element.process.call
+            end
+        end
+    end
+
+    def element_clicked
+        @buttons.each do |button|
+            return button if button.clicked?(mouse_x, mouse_y)
+        end
+        nil
+    end
+
+    def draw
+        @background_image.draw(0,0,0)
+        
+
+        if @state == :game
+            #@submarine.draw(0,0,3, scale_x = 0.1, scale_y = 0.1)
+            @submarine.draw(30,100,3)
+        elsif @state == :players
+            p "player"
+        elsif @state == :menu
+            @title.draw(150, 0, 1)
+            @background_image_2.draw(150, 140, 1)
+            #@button_instructions.draw(50, 300, 2)
+            @start_text.draw(280,405,3)
+            @button_start.image.draw(@button_start.position_x, @button_start.position_y, 2)
+        end
+    end
+
+
+
+
+    class Game
+
+        TYPES = [:triangular, :square, :pentagonal, :hexagonal]
+
+        attr_accessor :current_player
+
+        def initialize(players)
+            @players = players
+            @oxigen = 25
+            @step = 0
+            generate_treasures 
+            generate_positions
+            @current_player = @players.first
+        end
+
+        def update_oxigen
+            puts "updatin oxigen"
+            @players.each do |player|
+                @oxigen -= player.loot.count
+            end
+            if @oxigen == 0
+                check_dead_players
+            end
+        end
+
+        def check_dead_players
+            @players.each do |player|
+                if player.current_position != nil
+                    player.status = :dead
+                end
+            end
+        end
+
+        def calculate_movement
+            value = @current_player.current_dice[0] + @current_player.current_dice[1] - @current_player.loot.count
+            return 0 if value <= 0
+            value
+        end
+
+        def calculate_next_position
+            value = calculate_movement
+            current = @current_player.current_position.index
+            i = 1
+            j = 0
+            while i < value
+                if @positions[current + i + j].player.nil?
+                    i+=1
+                else
+                    j+=1
+                end
+            end
+            @current_player.desired_position = @positions[current + i + j]
+        end
+
+        def generate_treasures
+
+            @treasures = []
+            
+            i = 0
+            (0..15).each do |value|
+                type = TYPES[i/4]
+                @treasures.append(Treasure.new(type, value))
+                @treasures.append(Treasure.new(type, value))
+                i += 1
+            end
+            @treasures
+        end
+
+        def generate_positions
+
+            @positions = []
+
+            @treasures.each do |treasure|
+                @positions.append(Position.new(treasure))
+            end
+        end
+
+        def process(button_name = nil, options: nil)
+            #puts "#{@players.first.name}'s turn #{@players.first.color}"
+            puts @step
+            case @step
+            when 0
+                update_oxigen
+                @step += 1
+            when 1
+                if button_name == :direction
+                    @current_player.change_direction!
+                else
+                    @current_player.roll_dice
+                    @current_player.desired_position = calculate_next_position
+                    @step += 1
+                end
+            when 2
+                if button_name == :pick
+                    @current_player.loot.append(@positions[@current_player.current_position].treasure)
+                    @current_player.current_position.treasure = Treasure.new(:blank, nil)
+                elsif button_name == :drop
+                    @current_player.current_position.treasure = @current_player.loot[options[index_loot]] 
+                    #Revisar si extraer la logica de los pasos como botones visuales
+                end
+                next_player
+            end
+        end
+
+        def next_player
+            @current_player = @players[@players.index(@current_player) + 1]
+            if @current_player.nil?
+                @current_player = @players.first
+            end
+        end
+    end
+
+    class Player
+
+        attr_accessor :loot, :name, :color, :current_dice, :current_position, :desired_position
+
+        def initialize(name, color)
+            @name = name
+            @color = color
+            @direction = :down
+            @current_position = nil
+            @desired_position = nil
+            @loot = []
+            @status = :alive
+        end
+
+        def change_direction!
+            if @direction == :down
+                @direction = :up
+            else
+                @direction = :down
+            end
+        end
+
+        def roll_dice
+            @current_dice = [rand(1..3), rand(1..3)]
+        end
+
+    end
+
+    class Treasure
+
+        attr_accessor :type, :value
+
+        def initialize(type, value)
+            @type = type
+            @value = value
+            #@owner = nil
+        end
+
+    end
+
+    class Position
+
+        attr_accessor :treasure, :player
+
+        def initialize(treasure)
+            @treasure = treasure
+            @player = nil
+        end
+
+    end
+
+
+end
+
+DeepSeaAdventure.new.show
